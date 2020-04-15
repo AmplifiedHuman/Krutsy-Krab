9c1f180031698352bca6718ed90b328971730a6b
AmplifiedHuman <amplifiedhuman@gmail.com>
2020-04-15 23:58:18 +0800

    code cleanup
    Former-commit-id: 35492cca28ea8b5ed7dfc0bb1bb757fa11a2602c [formerly 753130bd2de320716efc970ee91b6605ba62097b] [formerly 23c5e0803dcf4ca618bbb9c8edee2ef101df1fab [formerly 8c9efc199ab639ff8ad02fbf71d5e7968d572af3]]
    Former-commit-id: 918ae0372794cd0eb58077e812c7ed78dbf7ac28 [formerly 9a6b751a7e7752d771e4c320cd2d827650a0758b]
    Former-commit-id: ab47b7e16bd4fbf299cce8a15c483efca95127a9

--- about.php
+++ about.php
@@ -2,14 +2,14 @@
 <html lang="en">
 
 <head>
-<?php 
+    <?php
     $pageTitle = 'About Us';
     include("meta.php");
-?>
+    ?>
 </head>
 
 <body class="lightpink">
-<?php include("header.html") ?>
+    <?php include("header.html") ?>
     <h1 class="generic-title team-title header-fix">Krusty Krab</h1>
     <div class="big-cake-container">
         <img src="img/big_cake.svg" alt="big-cake" class="generic-image">
@@ -32,25 +32,25 @@
                     <img src="img/person1.svg" class="generic-image">
                 </div>
                 <h2>Susan</h2>
-                <p>Baker</p>
+                <p class="normal-text">Baker</p>
             </div>
             <div class="team-entry">
                 <div class="team-image-container">
                     <img src="img/person2.svg" class="generic-image">
                 </div>
                 <h2>Michael</h2>
-                <p>Chef</p>
+                <p class="normal-text">Chef</p>
             </div>
             <div class="team-entry">
                 <div class="team-image-container">
                     <img src="img/person3.svg" class="generic-image">
                 </div>
                 <h2>Alex</h2>
-                <p>Waiter</p>
+                <p class="normal-text">Waiter</p>
             </div>
         </div>
     </section>
-<?php include("footer.html") ?>
+    <?php include("footer.html") ?>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 <html lang="en">
 
 <head>
-<?php 
+    <?php
     $pageTitle = 'Krusty Krab';
     include("meta.php");
-?>
+    ?>
 </head>
 
 <body class="lightpink">
-<?php include("header.html") ?>    
+    <?php include("header.html") ?>
     <div class="header-fix">
         <div class="cover parallax" id="hero-image">
             <h1 class="cover-text" id="covertitle">Krusty Krab</h1>
             <h1 class="cover-text">Taste of a lifetime.</h1>
         </div>
     </div>
-    <div class="lightpink normal-text intro-text">
+    <div class="normal-text intro-text">
         Our story began in 1994. Upon finishing their culinary studies in Paris, Gastón Acurio and Astrid Gutsche
         founded their
         eponymous restaurant in a small house in the district of Miraflores. At first, the restaurant was quite French,
@@ -26,18 +26,18 @@
     </div>
     <div class="lightpink generic-flex-container intro-container">
         <!-- TODO: add alt attribute -->
-        <div class="intro-entry">
-            <div class="intro-image-container">
+        <div class="intro-entry rounded">
+            <div class="intro-image-container rounded">
                 <img src="img/intro-1.jpg" class="generic-image rounded">
             </div>
         </div>
-        <div class="intro-entry">
-            <div class="intro-image-container">
+        <div class="intro-entry rounded">
+            <div class="intro-image-container rounded">
                 <img src="img/intro-2.jpg" class="generic-image rounded">
             </div>
         </div>
-        <div class="intro-entry">
-            <div class="intro-image-container">
+        <div class="intro-entry rounded">
+            <div class="intro-image-container rounded">
                 <img src="img/intro-3.jpg" id="intro-3" class="generic-image rounded">
             </div>
         </div>
@@ -46,37 +46,37 @@
     <div class="lightgrey product-container">
         <h1 class="generic-title">Our products</h1>
         <div class="generic-flex-container product-grid">
-            <div class="product-entry darkpink">
+            <div class="product-entry darkpink rounded">
                 <div class="product-image-container">
                     <img src="img/bread.jpg" alt="bread" class="generic-image rounded">
                 </div>
                 <h2 class="normal-text">Bread</h2>
             </div>
-            <div class="product-entry darkpink">
+            <div class="product-entry darkpink rounded">
                 <div class="product-image-container">
                     <img src="img/cakes.jpg" alt="cakes" class="generic-image rounded">
                 </div>
                 <h2 class="normal-text">Cakes</h2>
             </div>
-            <div class="product-entry darkpink">
+            <div class="product-entry darkpink rounded">
                 <div class="product-image-container">
                     <img src="img/coffee.jpg" alt="coffee" class="generic-image rounded">
                 </div>
                 <h2 class="normal-text">Coffee</h2>
             </div>
-            <div class="product-entry darkpink">
+            <div class="product-entry darkpink rounded">
                 <div class="product-image-container">
                     <img src="img/cupcakes.jpg" alt="cupcakes" class="generic-image rounded">
                 </div>
                 <h2 class="normal-text">Cupcakes</h2>
             </div>
-            <div class="product-entry darkpink">
+            <div class="product-entry darkpink rounded">
                 <div class="product-image-container">
                     <img src="img/desserts.jpg" alt="desserts" class="generic-image rounded">
                 </div>
                 <h2 class="normal-text">Desserts</h2>
             </div>
-            <div class="product-entry darkpink">
+            <div class="product-entry darkpink rounded">
                 <div class="product-image-container">
                     <img src="img/pastries.jpg" alt="pastries" class="generic-image rounded">
                 </div>
@@ -85,19 +85,17 @@
         </div>
     </div>
 
-    <h1 class="location-text">You can find us at</h1>
+    <h1 class="normal-text location-text">You can find us at</h1>
     <div class="generic-flex-container location-container">
         <div class="location-details">
             <h1>Bikini Bottom</h1>
             <p>University College Dublin, Belfield, Dublin 4, Ireland</p>
         </div>
-        <div class="gmap_canvas">
-            <iframe id="gmap_canvas"
-                src="https://maps.google.com/maps?q=Bermuda%20Triangle&t=&z=17&ie=UTF8&iwloc=&output=embed"
-                frameborder="0" scrolling="no" marginheight="0" marginwidth="0"> </iframe>
+        <div class="map-container">
+            <iframe src="https://maps.google.com/maps?q=Bermuda%20Triangle&t=&z=17&ie=UTF8&iwloc=&output=embed"></iframe>
         </div>
     </div>
-<?php include("footer.html") ?> 
+    <?php include("footer.html") ?>
 </body>
 
 </html>
