0b601d3801256b8cfe113f353d9579abb4e25392
AmplifiedHuman <amplifiedhuman@gmail.com>
2020-04-17 18:34:50 +0800

    added prices
    Former-commit-id: 3b4d4b684c74d0ff79bece68a6b456f47fa328e4 [formerly cf8c73a8ea673759152a0fb3037e351f69053314] [formerly 73e6c33dc43f64ec63899f646b6b2b5911253fdb [formerly dbfa5d63a4aff8baf1608b77f9f743c9725f6925]]
    Former-commit-id: 59451073636932b2a2956be5008eb07a18c07f97 [formerly aae6100b5b146e86ff965154e2e8f4a7d104e17e]
    Former-commit-id: d2430f17df162d30cb759e6ca76f02e784e11683

--- menu.php
+++ menu.php
@@ -24,36 +24,42 @@
                         <img src="img/menu/bread/bagel.jpg" class="generic-image" alt="bagel">
                     </div>
                     <h2 class="normal-text">Bagel</h2>
+                    <p class="normal-text price">€3</p>
                 </div>
                 <div class="menu-entry bread-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/bread/toast.jpg" class="generic-image" alt="toast">
                     </div>
                     <h2 class="normal-text">Toast</h2>
+                    <p class="normal-text price">€2.80</p>
                 </div>
                 <div class="menu-entry bread-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/bread/banana-bread.jpg" class="generic-image" alt="Banana Bread">
                     </div>
                     <h2 class="normal-text">Banana Bread</h2>
+                    <p class="normal-text price">€3.20</p>
                 </div>
                 <div class="menu-entry bread-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/bread/challah.jpg" class="generic-image" alt="challah">
                     </div>
                     <h2 class="normal-text">Challah</h2>
+                    <p class="normal-text price">€3</p>
                 </div>
                 <div class="menu-entry bread-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/bread/bun.jpg" class="generic-image" alt="bun">
                     </div>
                     <h2 class="normal-text">Bun</h2>
+                    <p class="normal-text price">€1.50</p>
                 </div>
                 <div class="menu-entry bread-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/bread/crossaint.jpg" class="generic-image" alt="crossaint">
                     </div>
                     <h2 class="normal-text">Crossaint</h2>
+                    <p class="normal-text price">€1.25</p>
                 </div>
             </div>
         </section>
@@ -66,54 +72,63 @@
                         <img src="img/menu/cakes/strawberry.jpg" class="generic-image rounded" alt="strawberry cake">
                     </div>
                     <h2 class="normal-text">Strawberry Cake</h2>
+                    <p class="normal-text price">€5.70</p>
                 </div>
                 <div class="menu-entry cake-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/cakes/cheese.jpg" class="generic-image rounded" alt="cheese cake">
                     </div>
                     <h2 class="normal-text">Cheese Cake</h2>
+                    <p class="normal-text price">€5.50</p>
                 </div>
                 <div class="menu-entry cake-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/cakes/sponge.jpg" class="generic-image rounded" alt="sponge cake">
                     </div>
                     <h2 class="normal-text">Sponge Cake</h2>
+                    <p class="normal-text price">€4.50</p>
                 </div>
                 <div class="menu-entry cake-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/cakes/carrot.jpg" class="generic-image rounded" alt="carrot cake">
                     </div>
                     <h2 class="normal-text">Carrot Cake</h2>
+                    <p class="normal-text price">€5.50</p>
                 </div>
                 <div class="menu-entry cake-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/cakes/red-velvet.jpg" class="generic-image rounded" alt="red velvet cake">
                     </div>
                     <h2 class="normal-text">Red Velvet</h2>
+                    <p class="normal-text price">€7</p>
                 </div>
                 <div class="menu-entry cake-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/cakes/blueberry.jpg" class="generic-image rounded" alt="blueberry cake">
                     </div>
                     <h2 class="normal-text">Blueberry Cake</h2>
+                    <p class="normal-text price">€5.70</p>
                 </div>
                 <div class="menu-entry cake-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/cakes/chocolate.jpg" class="generic-image rounded" alt="chocolate cake">
                     </div>
                     <h2 class="normal-text">Chocolate Cake</h2>
+                    <p class="normal-text price">€5.70</p>
                 </div>
                 <div class="menu-entry cake-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/cakes/tiramisu.jpg" class="generic-image rounded" alt="tiramisu cake">
                     </div>
                     <h2 class="normal-text">Tiramisu</h2>
+                    <p class="normal-text price">€6</p>
                 </div>
                 <div class="menu-entry cake-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/cakes/lemon.jpg" class="generic-image rounded" alt="lemon cake">
                     </div>
                     <h2 class="normal-text">Lemon Cake</h2>
+                    <p class="normal-text price">€5.50</p>
                 </div>
             </div>
         </section>
@@ -126,36 +141,42 @@
                         <img src="img/menu/coffee/americano.jpg" class="generic-image" alt="americano coffee">
                     </div>
                     <h2 class="normal-text">Americano</h2>
+                    <p class="normal-text price">€2.90</p>
                 </div>
                 <div class="menu-entry coffee-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/coffee/cappuccino.jpg" class="generic-image" alt="cappuccino coffee">
                     </div>
                     <h2 class="normal-text">Cappuccino</h2>
+                    <p class="normal-text price">€3.10</p>
                 </div>
                 <div class="menu-entry coffee-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/coffee/espresso.jpg" class="generic-image" alt="espresso coffee">
                     </div>
                     <h2 class="normal-text">Espresso</h2>
+                    <p class="normal-text price">€1.50</p>
                 </div>
                 <div class="menu-entry coffee-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/coffee/latte.jpg" class="generic-image" alt="latte coffee">
                     </div>
                     <h2 class="normal-text">Latte</h2>
+                    <p class="normal-text price">€2.90</p>
                 </div>
                 <div class="menu-entry coffee-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/coffee/matcha-latte.jpg" class="generic-image" alt="matcha latte coffee">
                     </div>
                     <h2 class="normal-text">Matcha Latte</h2>
+                    <p class="normal-text price">€3.10</p>
                 </div>
                 <div class="menu-entry coffee-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/coffee/mocha.jpg" class="generic-image" alt="mocha coffee">
                     </div>
                     <h2 class="normal-text">Mocha</h2>
+                    <p class="normal-text price">€3.10</p>
                 </div>
             </div>
         </section>
@@ -168,24 +189,28 @@
                         <img src="img/menu/cupcakes/strawberry.jpg" class="generic-image rounded" alt="strawberry cupcake">
                     </div>
                     <h2 class="normal-text">Strawberry Cupcake</h2>
+                    <p class="normal-text price">€1.50</p>
                 </div>
                 <div class="menu-entry cupcake-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/cupcakes/chocolate.jpg" class="generic-image rounded" alt="chocolate cupcake">
                     </div>
                     <h2 class="normal-text">Chocolate Cupcake</h2>
+                    <p class="normal-text price">€1.50</p>
                 </div>
                 <div class="menu-entry cupcake-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/cupcakes/mint.jpg" class="generic-image rounded" alt="mint cupcake">
                     </div>
                     <h2 class="normal-text">Mint Cupcake</h2>
+                    <p class="normal-text price">€1.30</p>
                 </div>
                 <div class="menu-entry cupcake-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/cupcakes/vanilla.jpg" class="generic-image rounded" alt="vanilla cupcake">
                     </div>
                     <h2 class="normal-text">Vanilla Cake</h2>
+                    <p class="normal-text price">€1.30</p>
                 </div>
             </div>
         </section>
@@ -198,30 +223,35 @@
                         <img src="img/menu/desserts/brownies.jpg" class="generic-image" alt="brownies">
                     </div>
                     <h2 class="normal-text">Brownies</h2>
+                    <p class="normal-text price">€1.30</p>
                 </div>
                 <div class="menu-entry dessert-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/desserts/cookies.jpg" class="generic-image" alt="cookies">
                     </div>
                     <h2 class="normal-text">Cookies</h2>
+                    <p class="normal-text price">€0.70</p>
                 </div>
                 <div class="menu-entry dessert-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/desserts/crepe.jpg" class="generic-image" alt="crepe">
                     </div>
                     <h2 class="normal-text">Crepe</h2>
+                    <p class="normal-text price">€5.20</p>
                 </div>
                 <div class="menu-entry dessert-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/desserts/doughnuts.jpg" class="generic-image" alt="doughnuts">
                     </div>
                     <h2 class="normal-text">Doughnuts</h2>
+                    <p class="normal-text price">€1.30</p>
                 </div>
                 <div class="menu-entry dessert-entry">
                     <div class="menu-image-container">
                         <img src="img/menu/desserts/pancakes.jpg" class="generic-image" alt="pancakes">
                     </div>
-                    <h2 class="normal-text">Matcha Latte</h2>
+                    <h2 class="normal-text">Pancakes</h2>
+                    <p class="normal-text price">€2.30</p>
                 </div>
             </div>
         </section>
