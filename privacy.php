db5636a203aec692f9fae5604b8137c41a1b6edc
AmplifiedHuman <amplifiedhuman@gmail.com>
2020-04-20 21:18:07 +0800

    added privacy page
    Former-commit-id: bfeec7f95fe6fb66efee31d0676bf5044f30af3d [formerly f37d23e9574cdfa3c89e00264a3f2989e1ce0b38] [formerly 21ede1d2bedbfd62f01dd7b670d511b9e5ce88dc [formerly 497b1274978bf63349e4051c819dc78113c2f7cd]]
    Former-commit-id: 4f6868150146530e346a814be272400fa47c1c89 [formerly 4e680f42f04d87ca9cc2126b87e3b829ffad7b8c]
    Former-commit-id: f67b092d419029c72355976dc34c98f34a878cb3

---
+++ privacy.php
@@ -0,0 +1,50 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    $pageTitle = 'Privacy';
+    include("meta.php");
+    ?>
+</head>
+
+<body class="lightpink">
+    <?php include("header.php") ?>
+    <h1 class="generic-title header-fix">Privacy</h1>
+    <div class="terms-container normal-text">
+        <p>
+            This website is provided to you on the following terms and conditions. By accessing any page on The Cake Shop Website,
+            you agree to be bound by these Terms and Conditions. These Terms and Conditions may be updated by The Krusty Krab from time to time without prior notice.
+        </p>
+        <h3>What information do we collect?</h3>
+        <p>
+            We collect information from you when you register or place an order on the KK website.
+            When ordering or registering on The KK Website, as appropriate, you may be asked to enter your: name,
+            e-mail address, mailing address, phone number or credit card information. You may, however, visit The KK Website anonymously.
+        </p>
+        <h3>What do we use your information for?</h3>
+        <p>
+            Your information, whether public or private, will not be sold, exchanged, transferred, or given to any other company for any reason whatsoever, without your consent,
+            other than for the express purpose of contacting us.
+        </p>
+        <h3>How do we protect your information?</h3>
+        <p>
+            We implement a variety of security measures to maintain the safety of your personal information when you enter
+            or submit your personal information.
+        </p>
+        <h3>Do we disclose any information to outside parties?</h3>
+        <p>
+            We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This does not include trusted third parties who assist us in operating our website, conducting our business, or servicing you, so long as those parties agree to keep this information confidential. We may also release your information when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or others rights, property, or safety. However, non-personally
+            identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.
+        </p>
+        <h3>Right to Amend our Privacy Policy Statement</h3>
+        <p>
+            The Cake Shop reserves the right to amend this Statement at any time. Changes will be effective immediately upon 
+            posting on The Cake Shop Website.
+        </p>
+    </div>
+    <?php include("footer.html") ?>
+</body>
+
+</html>
